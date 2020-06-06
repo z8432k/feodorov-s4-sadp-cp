45561af45e65531b4bd7b6e09649c0c1aff28f23
Alexander Feodorov <webmaster@unixcomp.org>
2020-06-06 20:15:25 +0300

    List cars & clients

---
+++ src/listcars.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <glib.h>
+
+#include "include/store.h"
+
+#define GETTEXT_PACKAGE "gtk20"
+#include <glib/gi18n-lib.h>
+
+int main(void)
+{
+  setlocale(LC_ALL, "");
+
+  printf("%s\n", cars_stringify(load_data()));
+}
--- src/store.c
+++ src/store.c
@@ -129,6 +129,32 @@ static inline void json_load_data(RawData_t *data)
   }
 }
 
+gchar* clients_stringify(RawData_t *data)
+{
+  gchar *str;
+  json_t *clients;
+
+  clients = clients_array_to_json_array(data->clients, json_build_client);
+  str = json_dumps(clients, JSON_INDENT(2));
+
+  json_decref(clients);
+
+  return str;
+}
+
+gchar* cars_stringify(RawData_t *data)
+{
+  gchar *str;
+  json_t *cars;
+
+  cars = cars_array_to_json_array(data->cars, json_build_car);
+  str = json_dumps(cars, JSON_INDENT(2));
+
+  json_decref(cars);
+
+  return str;
+}
+
 void save_data(RawData_t *data)
 {
   FILE *file = g_fopen(DATA_FILENAME, "w+");
